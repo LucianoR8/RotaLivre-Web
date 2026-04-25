524de2ec9579b03d12cac7bd436baa59263b3577
LucianoR8 <luciribsantos@gmail.com>
2026-04-24 22:45:29 -0300

    Corrigindo erro 401 do Authorize na PasseiosApiController

--- Rota-LivreWEB-API/Controllers/Api/PasseiosApiController.cs
+++ Rota-LivreWEB-API/Controllers/Api/PasseiosApiController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Rota_LivreWEB_API.Controllers.Api
 {
-    [Authorize]
+    // [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class PasseiosApiController : ControllerBase

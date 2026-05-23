12cad959768836c74d5d7b47c68d17ffae9d38c0
LucianoR8 <luciribsantos@gmail.com>
2026-05-23 00:01:13 -0300

    Inserção do charset UTF-8 para acentos e caracteres especiais na tela de redirecionamento de grupos

--- Rota-LivreWEB-API/Controllers/GrupoController.cs
+++ Rota-LivreWEB-API/Controllers/GrupoController.cs
@@ -21,6 +21,7 @@ namespace Rota_LivreWEB_API.Controllers
             var html = $@"
 <html>
 <head>
+    <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1' />
 
     <style>

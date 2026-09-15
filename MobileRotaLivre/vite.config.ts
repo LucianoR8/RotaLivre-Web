9972ccebc5706fa543a756823ce72e14c6c298e3
LucianoR8 <luciribsantos@gmail.com>
2026-09-14 23:23:56 -0300

    Alterando vite_confg para funcionamento do app web

--- MobileRotaLivre/vite.config.ts
+++ MobileRotaLivre/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  // ESSA É A LINHA MÁGICA PARA O CLOUDFLARE:
+  base: '/',
+  
   plugins: [react(), tailwindcss()],
 
   server: {
@@ -10,6 +13,9 @@ export default defineConfig({
     host: true,
     hmr: false,
 
+    // O proxy funciona apenas na sua máquina (localhost).
+    // No Cloudflare, certifique-se de que o React faz chamadas 
+    // com o link inteiro "https://rotalivre-web.onrender.com/api/..."
     proxy: {
       '/api': {
         target: 'https://rotalivre-web.onrender.com',
